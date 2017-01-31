3603aa338c64521c6adbe9e9241e5ba3dc04793a
Brandon Kiefer <brandon@teamspectator.com>
2017-01-30 20:17:31 -0600

    shitty wording

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hola, mundo!"
+    render html: "some fucking bullshit"
   end
 
   def goodbye
